6b135f4680ff5b51d5bf21b2217d456afde48753
rainasaxena <raina1.saxena@gmail.com>
2023-09-02 10:03:56 +0530

    made homepage responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,10 @@ const Hero = () => {
   return (
    <main className="Hero container">
 
+    <div div className="HeroImage">
+      <img src="/images/shoe_image.png" alt="" />
+    </div>
+
     <div className="HeroContent">
         <h1>NIKE.<br/>JUST DO IT.</h1>
         <p>Willpower knows no obstacles. Find your greatness. Strike fear or get struck. </p>
@@ -21,10 +25,6 @@ const Hero = () => {
           <a href="https://www.flipkart.com/mens-footwear/sports-shoes/nike~brand/pr?sid=all%2Cosp%2Ccil%2C1cu&page=13" target='blank'><img src="/images/flipkart.png" alt="" /></a>
           <a href="https://www.amazon.com/stores/page/1368A17F-31CB-40BB-8FF0-ED3D49148DBE" target='blank'><img src="/images/amazon.png" alt="" /></a>
         </div>
-    </div>
-
-    <div className="HeroImage">
-      <img src="/images/shoe_image.png" alt="" />
     </div>
 
    </main>
